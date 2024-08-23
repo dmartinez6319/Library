f23eec38d364d3d67c9a7cb316cfae850645df7f
Daniel Martinez <dmartinez6319@gmail.com>
2024-08-23 17:18:26 -0400

    HTML/CSS layout

---
+++ script.js
@@ -0,0 +1,35 @@
+const myLibrary = [];
+
+function Book(author, pages, title, read) {
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.title = title
+    this.tostring = () => {
+        return "hello"
+    }
+}
+
+function addBook() {
+
+}
+
+const book1 = new Book("Daniel",32,"The lost one",false)
+const book2 = new Book("Leinad",14,"Thalo",true)
+const book3 = new Book("Ynnad",33,"Three Musketeers",true)
+
+myLibrary.push(book1,book2,book3)
+console.log(myLibrary)
+console.log(book1)
+
+const x = document.createElement("p")
+let text = ""
+// for (let i in myLibrary) {
+//     text += myLibrary[i].author
+// }
+for (let i = 0;i < myLibrary.length;i++) {
+    text += `${myLibrary[i].author} of ${myLibrary[i].title} \n`
+}
+console.log(text)
+x.innerHTML = text
+document.body.append(x)
